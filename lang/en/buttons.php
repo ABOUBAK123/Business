4ee142b08ed2c981eee00dc937dbe6265c374f66
ABOUBAK123 <aboubak123@users.noreply.github.com>
2026-04-30 14:09:35 +0000

    feat(i18n): add buttons translation file (en, fr)

---
+++ lang/en/buttons.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'back' => 'Back',
+    'submit' => 'Submit',
+    'cancel' => 'Cancel',
+    'delete' => 'Delete',
+    'edit' => 'Edit',
+    'save' => 'Save',
+    'create' => 'Create',
+    'close' => 'Close',
+    'next' => 'Next',
+    'previous' => 'Previous',
+];
---
+++ lang/fr/buttons.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'back' => 'Retour',
+    'submit' => 'Soumettre',
+    'cancel' => 'Annuler',
+    'delete' => 'Supprimer',
+    'edit' => 'Modifier',
+    'save' => 'Enregistrer',
+    'create' => 'Créer',
+    'close' => 'Fermer',
+    'next' => 'Suivant',
+    'previous' => 'Précédent',
+];
